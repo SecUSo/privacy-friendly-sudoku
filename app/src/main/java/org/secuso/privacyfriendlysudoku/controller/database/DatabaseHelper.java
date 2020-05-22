9c31f0d869a27da1720dc77bf3da37aff07ed34b
uykek <uykek@student.kit.edu>
2020-05-22 14:50:59 +0200

    Update database version

--- app/src/main/java/org/secuso/privacyfriendlysudoku/controller/database/DatabaseHelper.java
+++ app/src/main/java/org/secuso/privacyfriendlysudoku/controller/database/DatabaseHelper.java
@@ -20,7 +20,7 @@ import java.util.List;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 
-    public static final int DATABASE_VERSION = 1;
+    public static final int DATABASE_VERSION = 2;
     public static final String DATABASE_NAME = "Database.db";
 
     public DatabaseHelper(Context context) {
