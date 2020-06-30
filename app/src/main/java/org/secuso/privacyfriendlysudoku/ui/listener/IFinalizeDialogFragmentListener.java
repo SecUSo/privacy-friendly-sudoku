8d19a2239df22fb7b7f265b0c97ddf4327e74e12
uykek <uykek@student.kit.edu>
2020-06-30 10:42:09 +0200

    Add a listener interface for the finalization dialog of the CreateSudokuActivity

---
+++ app/src/main/java/org/secuso/privacyfriendlysudoku/ui/listener/IFinalizeDialogFragmentListener.java
@@ -0,0 +1,6 @@
+package org.secuso.privacyfriendlysudoku.ui.listener;
+
+public interface IFinalizeDialogFragmentListener {
+    public void onFinalizeDialogPositiveClick(String input);
+    public void onDialogNegativeClick();
+}
