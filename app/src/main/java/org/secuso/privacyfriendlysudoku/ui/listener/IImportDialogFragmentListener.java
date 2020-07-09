f53b4ba3fa274969d515e919476e4a568f37e9dc
uykek <uykek@student.kit.edu>
2020-07-09 08:24:49 +0200

    Change IImportDialogFragmentListener interface

--- app/src/main/java/org/secuso/privacyfriendlysudoku/ui/listener/IImportDialogFragmentListener.java
+++ app/src/main/java/org/secuso/privacyfriendlysudoku/ui/listener/IImportDialogFragmentListener.java
@@ -1,4 +1,6 @@
 package org.secuso.privacyfriendlysudoku.ui.listener;
 
 public interface IImportDialogFragmentListener {
+    public void onImportDialogPositiveClick(String input);
+    public void onDialogNegativeClick();
 }
