1c6be6e5c4aa851b974ab679c59883755caa321c
uykek <uykek@student.kit.edu>
2020-07-14 09:29:13 +0200

    Add boolean 'isCustom' variable to GameInfoContainer and GameController

--- app/src/main/java/org/secuso/privacyfriendlysudoku/controller/helper/GameInfoContainer.java
+++ app/src/main/java/org/secuso/privacyfriendlysudoku/controller/helper/GameInfoContainer.java
@@ -25,6 +25,7 @@ public class GameInfoContainer {
     int[] setValues;
     boolean[][] setNotes;
     int hintsUsed;
+    boolean isCustom;
 
     public GameInfoContainer() {}
     public GameInfoContainer(int ID, GameDifficulty difficulty, GameType gameType, int[] fixedValues, int[] setValues, boolean[][] setNotes) {
@@ -40,11 +41,16 @@ public class GameInfoContainer {
         this.setValues = setValues;
         this.setNotes = setNotes;
         this.hintsUsed = hintsUsed;
+        isCustom = false;
     }
 
     public void setID(int ID) {
         this.ID = ID;
     }
+
+    public void setCustom (boolean isCustom) { this.isCustom = isCustom; }
+
+    public boolean isCustom () { return  isCustom; }
 
     public void parseGameType(String s) {
         gameType = Enum.valueOf(GameType.class, s);
